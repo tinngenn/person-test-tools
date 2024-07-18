c445b73c64e69817b91b691de38d981fea9a2b7d
tinngenn <tinngenn@qq.com>
2024-07-18 19:43:32 +0800

    更新kafka打印日志

--- person-test-tools-kafka/src/main/java/com/person/test/tools/kafka/Consumer/KafkaMessageConsumer.java
+++ person-test-tools-kafka/src/main/java/com/person/test/tools/kafka/Consumer/KafkaMessageConsumer.java
@@ -1,13 +1,16 @@
 package com.person.test.tools.kafka.Consumer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class KafkaMessageConsumer {
 
     @KafkaListener(topics = "admin-messages")
     public void receiveAdminMessage(String message) {
         System.out.println("Received message: " + message);
+        log.info("Received message: {}" ,message);
         // ...
     }
 }
